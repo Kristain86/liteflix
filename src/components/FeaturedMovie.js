7d9e5ba35fa19b8199931c962d0edd0ffd8dd059
Kristain86 <cristiansagula@gmail.com>
2019-08-30 19:29:38 -0300

    featured and navigation

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Navigation from './Navigation'
+import Navigation from './Navigation';
+import {ReactComponent as PlayIcon } from '../images/play.svg';
+import {ReactComponent as PlusIcon } from '../images/plus.svg';
 
 class FeaturedMovie extends React.Component {
   constructor() {
@@ -23,21 +25,20 @@ class FeaturedMovie extends React.Component {
           arrOfDates.push(fromDate);
         });
 
-        function sortDates(a, b)
-        {
-            return a.getTime() - b.getTime();
+        function sortDates(a, b) {
+          return a.getTime() - b.getTime();
         }
         var sorted = arrOfDates.sort(sortDates);
-        var maxDate = sorted[sorted.length-1];
+        var maxDate = sorted[sorted.length - 1];
 
         const formatMaxDate = Date.parse(maxDate)
 
         data.results.forEach((elem, index) => {
 
-           const parsed_release = Date.parse(elem.release_date);
+          const parsed_release = Date.parse(elem.release_date);
 
-          if( parsed_release === formatMaxDate){
-            this.setState({ lastMovie: data.results[index]})
+          if (parsed_release === formatMaxDate) {
+            this.setState({ lastMovie: data.results[index] })
           }
         });
         console.log('state', this.state.lastMovie)
@@ -46,12 +47,27 @@ class FeaturedMovie extends React.Component {
   }
 
   render() {
-
+    const urlImg = 'https://image.tmdb.org/t/p/original/'
 
     return (
-      <div className="hero">
+      <div className="hero" style={{backgroundImage: "url(" + urlImg + this.state.lastMovie.backdrop_path + ")"}} >
         <div className="main-container">
           <Navigation />
+          <section className="featured-movie">
+            <article>
+              <h3 className="highlight-title uppercase">Original de <b>Liteflix</b></h3>
+              <h1 className="movie-title roboto-slab">{this.state.lastMovie.title}</h1>
+              <div className="cta-buttons">
+                <button className="play-btn"><PlayIcon />Reproducir</button>
+                <button><PlusIcon />Mi lista</button>
+              </div>
+              <div className="description-featured">
+                <h4>Ver temporada 1</h4>
+                <p>{this.state.lastMovie.overview}</p>
+              </div>
+
+            </article>
+          </section>
         </div>
       </div>
     )
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { ReactComponent as LiteflixLogo } from '../images/liteflix.svg';
+import { ReactComponent as PlusIcon } from '../images/plus.svg';
+import { ReactComponent as BellIcon } from '../images/bell.svg';
+import { ReactComponent as User01 } from '../images/user-01.svg';
+import { ReactComponent as Arrow } from '../images/arrow.svg';
+
 
 class Navigation extends React.Component {
   constructor() {
@@ -12,21 +17,36 @@ class Navigation extends React.Component {
       <div className="nav-container">
         <nav className="main-nav">
           <ul>
-            <li><a href="./"><LiteflixLogo /></a></li>
-            <li><a href="./">Inicio</a></li>
+            <li><a href="./" className="logo-liteflix"><LiteflixLogo /></a></li>
+            <li><a href="./"><b>Inicio</b></a></li>
             <li><a href="./">Series</a></li>
             <li><a href="./">Películas</a></li>
             <li><a href="./">Agregados recientemente</a></li>
             <li><a href="./">Mi lista</a></li>
-            <li><a href="./">+</a></li>
+            <li><a href="./" className="add-movie-btn"><PlusIcon /><span>Agregar película</span></a></li>
           </ul>
         </nav>
 
         <nav className="sideNav">
           <ul>
-            <li><a href="./">asdasd</a></li>
-            <li><a href="./">asdasd</a></li>
-            <li><a href="./">asdasd</a></li>
+            <li><a href="./">Niños</a></li>
+            <li><a href="./" className="notifications"><BellIcon /></a></li>
+            <li>
+              <span className="accout"><User01 /><Arrow />
+              <div className="account-panel">
+                  <ul>
+                    <li><a href="./"><User01 /><span>Cristian Sagula</span></a></li>
+                    <li><a href="./"><User01 /><span>Indiana Jones</span></a></li>
+                    <li><a href="./"><User01 /><span>Walter White</span></a></li>
+                  </ul>
+                  <ul className="setting-list">
+                    <li><a href="./">Configuración</a></li>
+                    <li><a href="./">Ayuda</a></li>
+                    <li><a href="./">Log out</a></li>
+                  </ul>
+                  </div>
+              </span>
+            </li>
           </ul>
         </nav>
       </div>
